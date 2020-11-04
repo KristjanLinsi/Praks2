28ae22d1a983717d0886ff2a96a7ee192ac5f9d0
Kristjan Linsi <kristjan.linsi@khk.ee>
2020-11-04 11:50:01 +0200

    praks2 Tervitus

---
+++ yl1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Koosta script, mis väljastab kasutajale tervitust “Tere, kasutajanimi !”.
+
+
+#echo "Tere kasutajanimi!"
+# Yl1
+# $User näitab kasutajat kes presgu on sees.
+echo "Tere, $USER !"
+echo "!"
+
+
+
+
+
